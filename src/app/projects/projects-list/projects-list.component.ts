5868b0fdd91746b238b0cc302bc8a90ff3ea256d
Alex Mac <meet.alexmac@gmail.com>
2017-12-08 23:53:21 -0800

    Update portfolio projects

--- src/app/projects/projects-list/projects-list.component.ts
+++ src/app/projects/projects-list/projects-list.component.ts
@@ -9,18 +9,6 @@ import { Project } from '../project.model';
 export class ProjectsListComponent implements OnInit {
 	projects: Project[] = [
 		new Project(
-			"Simms Fishing", 
-			"simms-fishing",
-			"assets/simms-fishing.png",
-			[
-				"Implemented core templates and front end logic in Magento 2.", 
-            	"Implement client provided designs in Magento 2.",
-            	"Update existing functionality in PHP and Magento 1.9",
-            	"Fix existing bugs on both the front end and the backend",
-
-			], 
-			"https://simmsfishing.com"),
-		new Project(
 			"Online Go", 
 			"online-go",
 			"",
@@ -39,18 +27,7 @@ export class ProjectsListComponent implements OnInit {
                 "Developed and quality tested the end-to-end user experience.", 
                 "I primarily worked on the front end business logic"
             ], 
-			"http://joecoffeeapp.com/"),
-		new Project(
-			"Farm Bureau", 
-			"farm-bureau",
-			"",
-			[
-				"Programmed the front end logic and behavior in AngularJs.",
-				"Heavily upgrade proprietary api architecture and integration.",
-                "Communicated technical ideas to non-technical clients."
-            ], 
-            "https://scorecard.fb.org/home"),
-
+			"http://joecoffeeapp.com/")
 	];
 
 	constructor() { }
