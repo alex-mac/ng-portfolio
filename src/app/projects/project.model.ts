ab2f68d49ffec6d80d9171d6a760ca441bdcd08c
Alex Mac <meet.alexmac@gmail.com>
2017-12-09 00:08:07 -0800

    lint files

--- src/app/projects/project.model.ts
+++ src/app/projects/project.model.ts
@@ -1,18 +1,18 @@
 export class Project {
-	public name: string;
-	public id: string;
-	public description: string[];
-	public projectUrl: string;
-	public logoPath: string;
-	public showLogo: string;
+    public name: string;
+    public id: string;
+    public description: string[];
+    public projectUrl: string;
+    public logoPath: string;
+    public showLogo: string;
 
-	constructor(name: string, id: string, logo: string, description: string[], projectUrl: string) {
-		this.name = name;
-		this.id = id;
-		this.logoPath = logo;
-		this.description = description;
-		this.projectUrl = projectUrl;
-		this.showLogo = logo === "" ? "hide" : "show";
-		console.log(logo === "");
-	}
+    constructor(name: string, id: string, logo: string, description: string[], projectUrl: string) {
+        this.name = name;
+        this.id = id;
+        this.logoPath = logo;
+        this.description = description;
+        this.projectUrl = projectUrl;
+        this.showLogo = logo === '' ? 'hide' : 'show';
+        console.log(logo === '');
+    }
 }
--- src/app/projects/projects-list/projects-list.component.ts
+++ src/app/projects/projects-list/projects-list.component.ts
@@ -7,36 +7,35 @@ import { Project } from '../project.model';
   styleUrls: ['./projects-list.component.css']
 })
 export class ProjectsListComponent implements OnInit {
-	projects: Project[] = [
-		new Project(
-			"Online Go", 
-			"online-go",
-			"",
-			[
-				"When I'm not playing Go, I'm contributing to the open sourced online go server.", 
-                "Update the user interface and upgrade the user experience by addressing bugs.", 
-                "Actively play using the web portal, keeping my eye out for areas that can improve."
-            ], 
-			"http://online-go.com"),
-		new Project(
-			"Joe Coffee", 
-			"joe-coffee",
-			"assets/joe-coffee-app.png",
-			[
-				"Developed business logic that shows users nearby coffee shops, sorted from nearest to furthest.", 
-                "Developed and quality tested the end-to-end user experience.", 
-                "I primarily worked on the front end business logic"
-            ], 
-			"http://joecoffeeapp.com/")
-	];
+    projects: Project[] = [
+        new Project(
+            'Online Go',
+            'online-go',
+            '',
+            [
+                'When I am not playing Go, I am contributing to the open sourced online go server.',
+                'Update the user interface and upgrade the user experience by addressing bugs.',
+                'Actively play using the web portal, keeping my eye out for areas that can improve.'
+            ],
+            'http://online-go.com'),
+        new Project(
+            'Joe Coffee',
+            'joe-coffee',
+            'assets/joe-coffee-app.png',
+            [
+            'Developed business logic that shows users nearby coffee shops, sorted from nearest to furthest.',
+                'Developed and quality tested the end-to-end user experience.',
+                'I primarily worked on the front end business logic'
+            ],
+            'http://joecoffeeapp.com/')
+    ];
 
-	constructor() { }
+    constructor() { }
 
-	ngOnInit() {
-  	}
+    ngOnInit() {
+    }
 
-  	openProject(url: string) {
-  		window.open(url);  			
-  	}
-
+    openProject(url: string) {
+        window.open(url);
+    }
 }
